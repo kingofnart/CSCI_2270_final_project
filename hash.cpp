850c2705b0bdb0aae42ad91b6ea1919cb6d7db59
kingofnart <49491343+kingofnart@users.noreply.github.com>
2019-04-20 23:26:09 -0600

    Updated node struct and functions accordingly

--- hash.cpp
+++ hash.cpp
@@ -1,21 +1,10 @@
 #include "hash.hpp"
 #include <iostream>
-#include <cmath>
-#include <cstdlib>
-#include <unistd.h>
-#include <stdio.h>
-#include <string>
-#include <fstream>
-#include <vector>
-#include <string>
-#include <iomanip>
-#include <sstream>
-#include <iostream>
 
 
 HashTable :: HashTable(int newHashTableSize){
     hashTableSize = newHashTableSize;
-    hashTable = new vocab*[hashTableSize];
+    hashTable = new HNode*[hashTableSize];
     numItems = 0;
     for(int i = 0; i < hashTableSize; i++){
         hashTable[i] = nullptr;
@@ -24,38 +13,36 @@ HashTable :: HashTable(int newHashTableSize){
 
 HashTable :: ~HashTable(){
     for(int i = 0; i < hashTableSize; i++){
-        vocab* entry = hashTable[i];
+        HNode* entry = hashTable[i];
         while(entry != nullptr){
-            vocab* previous = entry;
-            entry = entry -> next;
+            HNode* previous = entry;
+            entry = entry->next;
             delete previous;
         }
     }
     delete[] hashTable;
 }
 
-void HashTable :: addWord(string word){
+void HashTable :: addWord(string word, string correctSpelling){
     unsigned int hashValue = getHash(word);
     if(!searchTable(word)){
-        vocab* newVocab = new vocab;
-        newVocab->word = word;
-        newVocab->mispelling = mispelling;
-        newVocab->next = nullptr;
-        
+        HNode* newHNode = new HNode;
+        newHNode->word = word;
+        newHNode->correctSpelling = correctSpelling;
+        newHNode->next = nullptr;
+
         if(hashTable[hashValue] == nullptr){
-            hashTable[hashValue] = newVocab;
+            hashTable[hashValue] = newHNode;
         }
         else{
-            newVocab->next = hashTable[hashValue];
-            hashTable[hashValue] = newVocab;
+            newHNode->next = hashTable[hashValue];
+            hashTable[hashValue] = newHNode;
         }
-        numItems++;
     }
 }
 
 bool HashTable :: isInTable(string word){
-    vocab* temp = searchTable(word);
-    
+    HNode* temp = searchTable(word);
     if(temp != nullptr){
         return true;
     }
@@ -64,36 +51,28 @@ bool HashTable :: isInTable(string word){
     }
 }
 
-unsigned int HashTable :: getHash(string word){ //if this the hash we want to use? It is supposed to be most efficient 
+unsigned int HashTable :: getHash(string word){ //if this the hash we want to use? It is supposed to be most efficient
     unsigned int hash = 5381;
-    
     for(unsigned int i = 0; i < word.length(); i++){
         hash = hash*33 + word[i];
     }
-
     hash = hash % hashTableSize;
-    
     return hash;
 }
 
-vocab* searchItem(string key, vocab** table, int hashTableSize){ //helper function to search
-    unsigned int hashValue = 5381;
-    
-    for(unsigned int i = 0; i < key.length(); i++){
-        hashValue = hashValue*33 + key[i];
+HNode* HashTable::searchItem(string key, HNode** table){ //helper function to search
+  int hashValue = getHash(key);
+  HNode* temp = table[hashValue]; //this pointer points at the head
+  while(temp != nullptr){
+    //cout << temp->word << " ";
+    if(temp->word == key){ //once you find the pointer with the correct word then it's good
+        return temp;
     }
-    hashValue = hashValue % hashTableSize;
-    
-    vocab* temp = table[hashValue]; //this pointer points at the head 
-    while(temp != nullptr){
-        if(temp->word == key){ //once you find the pointer with the correct word then it's good
-            return temp;
-        }
-        temp = temp -> next; //keep traversing till you find it
-    }
-    return nullptr;
+    temp = temp-> next; //keep traversing till you find it
+  }
+  return nullptr;
 }
 
-vocab* HashTable :: searchTable(string word){
-    return searchItem(word, hashTable, hashTableSize);
+HNode* HashTable :: searchTable(string word){
+    return searchItem(word, hashTable);
 }
