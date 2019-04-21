7a83b5c983683bb944cafd216bdab4ae8ff9b657
kingofnart <49491343+kingofnart@users.noreply.github.com>
2019-04-20 23:26:56 -0600

    Updated functions for new node struct

--- hash.hpp
+++ hash.hpp
@@ -1,30 +1,29 @@
 #include <string>
-#include <cstring>
 
 using namespace std;
 
-struct vocab // Vocab Record
+struct HNode
 {
     string word; // The Word
-    vocab* next; // The Next Word
+    string correctSpelling; // misspelled word
+    HNode* next; // The Next Word
 };
 
 class HashTable {
   public:
     HashTable(int newHashTableSize);
     ~HashTable();
-    void addWord(string word, string mispelling); // Add a Vocab Word
+    void addWord(string word, string correctSpelling); // Add a Vocab Word
     bool isInTable(string word); // Search for a Word in Vocabulary
+    HNode* searchTable(string word); // Search for the location of a word
 
   private:
     /* member functions */
     unsigned int getHash(string word); // The Hashing Function
-    vocab* searchTable(string word); // Search for the location of a word
-    
-    //////Probably not Needed//////////////
+    HNode* searchItem(string key, HNode** table);
+
     /* instance variables */
-    // vocab** hashTable;
-    vocab** hashTable; // Hashtable of vocab word (possibly a pointer) I think it needs to be a pointer but unsure 
+    HNode** hashTable; // Hashtable of vocab word (possibly a pointer) I think it needs to be a pointer but unsure
     int hashTableSize; // The Table's Size
     int numItems;
 };
